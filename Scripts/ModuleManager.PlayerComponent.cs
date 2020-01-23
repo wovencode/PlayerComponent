d3dde9d91f37da1c8e287847662fd0aae6f2ca82
Fhiz <support@wovencode.net>
2020-01-23 17:39:23 +0100

    Updated Define to ModuleManager

---
+++ Scripts/ModuleManager.PlayerComponent.cs
@@ -0,0 +1,52 @@
+// =======================================================================================
+// Wovencore
+// by Weaver (Fhiz)
+// MIT licensed
+// =======================================================================================
+
+#if UNITY_EDITOR
+
+using Wovencode;
+using Wovencode.Modules;
+using UnityEditor;
+using UnityEngine;
+
+namespace Wovencode.Modules
+{
+	
+	// ===================================================================================
+	// ModuleManager
+	// ===================================================================================
+	public partial class ModuleManager
+	{
+		
+		// -------------------------------------------------------------------------------
+		// Constructor
+		// -------------------------------------------------------------------------------
+		[DevExtMethods("Constructor")]
+		public static void Constructor_PlayerComponent()
+		{
+			
+			Module module = new Module();
+			
+			module.name				= "PlayerComponent";
+			module.define			= "wPLAYER";
+			module.version       	= "1.0";
+			module.unity3dVersion 	= "2018.x-2019.x";
+			module.nameSpace		= "Wovencode";
+        	module.package         	= "Wovencore";
+        	module.author        	= "Fhiz";
+        	module.dependencies  	= "wTOOLS,wDB,wNETWORK,wSYNCABLE,wTEMPLATES";
+       		module.comments      	= "none";
+        	
+        	AddModule(module);
+        	
+		}
+
+	}
+
+}
+
+#endif
+
+// =======================================================================================
